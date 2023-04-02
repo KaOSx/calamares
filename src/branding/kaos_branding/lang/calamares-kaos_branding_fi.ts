fc8762c25fd08315bc2ed24f5fb5674818ff9a77
demmm <anke62@gmail.com>
2023-04-02 13:43:53 +0200

    merge transifex branding, add fi

--- src/branding/kaos_branding/lang/calamares-kaos_branding_fi.ts
+++ src/branding/kaos_branding/lang/calamares-kaos_branding_fi.ts
@@ -1,55 +1,55 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="tr_TR">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="fi">
 <context>
     <name>show</name>
     <message>
         <location filename="../show.qml" line="72"/>
         <source>installation</source>
-        <translation>kurulum</translation>
+        <translation>asennus</translation>
     </message>
     <message>
         <location filename="../show.qml" line="73"/>
         <source>After creating your chosen disk setup in the first 10 % the full copying of the ISO will take the longest of this install phase and will run until approximately 45%.</source>
-        <translation>İlk %10&apos; daki seçilen disk kurulumunu oluşturduktan sonra ISO&apos;nun tam kopyalanması bu yükleme aşamasının en uzun süren aşaması olacak ve yaklaşık %45 düzeyine dek çalışacaktır.</translation>
+        <translation>Kun valitsemasi levyasetukset on luotu ensimmäinen 10 % tehty, ISO:n täydellinen kopiointi kestää pisimmillään tästä asennusvaiheesta noin 45 % asti.</translation>
     </message>
     <message>
         <location filename="../show.qml" line="76"/>
         <source>modules</source>
-        <translation>modüller</translation>
+        <translation>moduulit</translation>
     </message>
     <message>
         <location filename="../show.qml" line="77"/>
         <source>Once the ISO is copied some 25 post-install modules will run. This includes setting user specific options, removing Live Session only packages and adjusting hardware setup.</source>
-        <translation>ISO kopyalanınca 25 kadar yükleme sonrası modülü çalışacaktır. Bu, kullanıcıya özel seçeneklerin kurulumu, Canlı Oturum paketlerinin kaldırılması ve donanım kurulumunun ayarlanmasını içerir. </translation>
+        <translation>Kun ISO on kopioitu, noin 25 asennuksen jälkeistä moduulia suoritetaan. Tähän sisältyy käyttäjäkohtaisten asetusten määrittäminen, vain Live Session -pakettien poistaminen ja laitteistoasetusten säätäminen.</translation>
     </message>
     <message>
         <location filename="../show.qml" line="80"/>
         <source>office suites</source>
-        <translation>ofis takımları</translation>
+        <translation>toimistopaketit</translation>
     </message>
     <message>
         <location filename="../show.qml" line="81"/>
         <source>The default Office Suite is LibreOffice.</source>
-        <translation>Varsayılan ofis takımı LibreOffice.</translation>
+        <translation>Oletus toimistopaketti on LibreOffice.</translation>
     </message>
     <message>
         <location filename="../show.qml" line="82"/>
         <source>Calligra is available in the repositories</source>
-        <translation>Calligra depolarda mevcuttur</translation>
+        <translation>Calligra on saatavilla arkistosta</translation>
     </message>
     <message>
         <location filename="../show.qml" line="85"/>
         <source>Package Management</source>
-        <translation>Paket Yönetimi</translation>
+        <translation>Pakettien hallinta</translation>
     </message>
     <message>
         <location filename="../show.qml" line="86"/>
         <source>For package management Octopi is the GUI application.</source>
-        <translation>Octopi paket yönetimi için grafiksel arayüz uygulamasıdır.</translation>
+        <translation>Pakettien hallintaan käyttöliittymä on Octopi</translation>
     </message>
     <message>
         <location filename="../show.qml" line="87"/>
         <source>Pacman is the cli application.</source>
-        <translation>Pacman komut satırı uygulamasıdır.</translation>
+        <translation>Pacman on sovellus päätteeseen.</translation>
     </message>
     <message>
         <location filename="../show.qml" line="90"/>
@@ -59,22 +59,22 @@
     <message>
         <location filename="../show.qml" line="91"/>
         <source>Qt/KDE specific internet applications include the Falkon web-browser, KDE Connect for device Synchronization, Kaidan for chat  and NeoChat, the Matrix client.</source>
-        <translation>Qt/KDE kaynaklı internet uygulamaları Falkon web-tarayıcı, Aygıt eşitlemesi için KDE Connect, sohbet için Kaidan  ve NeoChat, Matrix istemcilerini içerir.</translation>
+        <translation>Qt/KDE-ominaisia ohjelmia ovat Falkon-selain, KDE Connect laitteiden synkronointia varten, Kaidan chatille ja NeoChat Matrix:lle.</translation>
     </message>
     <message>
         <location filename="../show.qml" line="94"/>
         <source>enjoy</source>
-        <translation>tadını çıkarın</translation>
+        <translation>pidä hauskaa</translation>
     </message>
     <message>
         <location filename="../show.qml" line="95"/>
         <source>May using KaOS be a pleasant experience for you.</source>
-        <translation>KaOS kullanmak sizin için hoş bir deneyim olabilir.</translation>
+        <translation>KaOS voi olla sinulle miellyttävä kokemus.</translation>
     </message>
     <message>
         <location filename="../show.qml" line="96"/>
         <source>Don&apos;t hesitate to give your opinion about KaOS in the Forum</source>
-        <translation>KaOS hakkındaki fikirlerinizi Forum&apos;da belirtmekten çekinmeyin</translation>
+        <translation>Älä epäröi kertoa mielipidettäsi KaOS-foorumilla</translation>
     </message>
 </context>
 </TS>
